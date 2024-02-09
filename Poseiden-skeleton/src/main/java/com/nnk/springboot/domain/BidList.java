d3dbfe4909dac2fbd9210bf6d3c3526b14ed9df6
Mourad-x64 <95309482+Mourad-x64@users.noreply.github.com>
2024-02-09 20:24:29 +0100

    Initial commit.

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,15 +1,14 @@
 package com.nnk.springboot.domain;
 
-import org.springframework.beans.factory.annotation.Required;
+import jakarta.persistence.*;
 
-import javax.persistence.*;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotBlank;
-import java.sql.Date;
-import java.sql.Timestamp;
 
 @Entity
 @Table(name = "bidlist")
 public class BidList {
     // TODO: Map columns in data table BIDLIST with corresponding java fields
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -1,12 +1,13 @@
 package com.nnk.springboot.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.sql.Timestamp;
+import jakarta.persistence.*;
+
 
 @Entity
 @Table(name = "rating")
 public class Rating {
     // TODO: Map columns in data table RATING with corresponding java fields
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
 }
